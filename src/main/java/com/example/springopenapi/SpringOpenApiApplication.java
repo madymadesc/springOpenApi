7f8db52de2cb70c67cb16e51ed1bfc5e83339d3e
madymadesc <madymadesc@gmail.com>
2022-12-02 23:42:17 +0000

    creation du projet springOpenApi

---
+++ src/main/java/com/example/springopenapi/SpringOpenApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOpenApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springopenapi/SpringOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
