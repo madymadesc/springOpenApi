169db7a3bc86e6d2a1030be0fe4c026f1bf0adcc
madymadesc <madymadesc@gmail.com>
2022-12-04 15:29:43 +0000

    ajoute de la dependance openapi

--- src/main/java/com/example/springopenapi/service/AccountService.java
+++ src/main/java/com/example/springopenapi/service/AccountService.java
@@ -1,7 +1,23 @@
 package com.example.springopenapi.service;
 
+import com.example.springopenapi.models.Account;
+import com.example.springopenapi.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountService {
+    @Autowired
+    AccountRepository accountRepository;
+
+    public Account addAccount(Account account){
+        return accountRepository.save(account);
+    }
+
+    public List<Account> listAccount(){
+        return  accountRepository.findAll();
+    }
+
 }
