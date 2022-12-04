8a6142a6e0d705d8565e5d8e18efeade2df784ec
madymadesc <madymadesc@gmail.com>
2022-12-04 15:37:25 +0000

    configuration de applicion

--- src/main/java/com/example/springopenapi/SpringOpenApiApplication.java
+++ src/main/java/com/example/springopenapi/SpringOpenApiApplication.java
@@ -1,9 +1,12 @@
 package com.example.springopenapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "OPEN API",version = "1.0.0",description = "Exemple de projet "))
 public class SpringOpenApiApplication {
 
     public static void main(String[] args) {
